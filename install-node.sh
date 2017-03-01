fc78f7b0220a4cdd79715af6e2aa38750778166a
Elson Rodriguez <elson.rodriguez@gmail.com>
2017-02-28 22:04:41 -0800

    Working properly, node registers.

--- install-node.sh
+++ install-node.sh
@@ -1,15 +1,19 @@
-cp bin/{kubectl,kube-proxy} /usr/bin
+#!/bin/bash -x
+cp bin/{kubelet,kube-proxy} /usr/bin
 
 cp units/{kubelet.service,kube-proxy.service} /etc/systemd/system/
 
 mkdir -p /etc/kubernetes/node
+mkdir -p /etc/kubernetes/manifests
+
 cp conf/{kubelet.conf,kube-proxy.conf} /etc/kubernetes/node
 
-if [ -z "$1"] then
-  sed -i "/http:\/\/master/s/master/$1/" /etc/kubernetes/kubelet.conf
-  sed -i "/http:\/\/master/s/master/$1/" /etc/kubernetes/kube-proxy.conf
+if [ -n "${1}" ]; then
+  echo "Overriding default master url"
+  sed -i "/http:\/\/master/s/master/$1/" /etc/kubernetes/node/kubelet.conf
+  sed -i "/http:\/\/master/s/\/master/\/$1/" /etc/kubernetes/node/kube-proxy.conf
 fi
 
-systemctl daemon reload
+systemctl daemon-reload
 systemctl enable kubelet kube-proxy 
 systemctl start kubelet kube-proxy 
