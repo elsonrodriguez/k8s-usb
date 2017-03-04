a270a07b95e00d0798eab44edc1015fb1b98ab3a
Elson Rodriguez <elson.rodriguez@gmail.com>
2017-03-03 21:13:15 -0800

    Updating download script.

--- download-binaries.sh
+++ download-binaries.sh
@@ -1,9 +1,28 @@
-#stub
+#!/bin/bash
 
-K8S_VERSION=${K8S_VERSION:-1.2.0}
-curl -L -O https://storage.googleapis.com/kubernetes-release/release/v$K8S_VERSION/kubernetes.tar.gz -z kubernetes.tar.gz
+K8S_VERSION=${K8S_VERSION:-1.5.2}
+DOCKER_VERSION=${DOCKER_VERSION:-1.12.1}
+ETCD_VERSION=${ETCD_VERSION:-3.0.15}
 
+mkdir -p bin
+cd bin
 
-ETCD_VERSION=2.3.0
-curl -L -O https://github.com/coreos/etcd/releases/download/v$ETCD_VERSION/etcd-v$ETCD_VERSION-linux-amd64.tar.gz -z etcd-v$ETCD_VERSION-linux-amd64.tar.gz
+curl -L -O https://storage.googleapis.com/kubernetes-release/release/v${K8S_VERSION}/bin/linux/amd64/kubectl 
+curl -L -O https://storage.googleapis.com/kubernetes-release/release/v${K8S_VERSION}/bin/linux/amd64/kube-apiserver
+curl -L -O https://storage.googleapis.com/kubernetes-release/release/v${K8S_VERSION}/bin/linux/amd64/kube-controller-manager
+curl -L -O https://storage.googleapis.com/kubernetes-release/release/v${K8S_VERSION}/bin/linux/amd64/kube-scheduler
+curl -L -O https://storage.googleapis.com/kubernetes-release/release/v${K8S_VERSION}/bin/linux/amd64/kubelet 
+curl -L -O https://storage.googleapis.com/kubernetes-release/release/v${K8S_VERSION}/bin/linux/amd64/kube-proxy
+
+curl -L -O https://get.docker.com/builds/Linux/x86_64/docker-${DOCKER_VERSION}.tgz
+
+tar -zxvf docker-${DOCKER_VERSION}.tgz
+sudo cp docker/docker* .
+rm -rf docker
+rm docker-${DOCKER_VERSION}.tgz 
+
+curl -L -O https://github.com/coreos/etcd/releases/download/v${ETCD_VERSION}/etcd-v${ETCD_VERSION}-linux-amd64.tar.gz
+tar -zxvf etcd-v${ETCD_VERSION}-linux-amd64.tar.gz
+cp etcd-v${ETCD_VERSION}-linux-amd64/etcd* .
+rm -rf etcd-v${ETCD_VERSION}-linux-amd6
 
