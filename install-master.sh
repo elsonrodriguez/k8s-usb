dcee0f23451e03aad2fef76788639d88cf64eff9
Elson Rodriguez <elson.rodriguez@gmail.com>
2017-03-04 18:27:30 -0800

    Adding kube-dns

--- install-master.sh
+++ install-master.sh
@@ -2,8 +2,15 @@ cp bin/{etcdctl,etcd,kubectl,kube-apiserver,kube-controller-manager,kube-schedul
 
 cp units/{etcd.service,kube-apiserver.service,kube-controller-manager.service,kube-scheduler.service} /etc/systemd/system/
 
-./generate-kube-routes.sh
-cp units/kube-routes.network /etc/systemd/network
+#Systemd routes not working, using hack for now
+#./generate-kube-routes.sh
+#cp units/kube-routes.network /etc/systemd/network
+./generate-kube-routes-script.sh
+cp units/kube-routes.service /etc/systemd/system/
+cp units/kube-routes.sh /usr/local/bin
+chmod +x /usr/local/bin/kube-routes.sh
+systemctl enable kube-routes
+systemctl start kube-routes
 
 mkdir -p /etc/kubernetes/master
 cp conf/{config.conf,apiserver.conf,controller-manager.conf,scheduler.conf} /etc/kubernetes/master
@@ -19,3 +26,7 @@ openssl genrsa -out /srv/kubernetes/kube-serviceaccount.key
 systemctl daemon-reload
 systemctl enable etcd kube-apiserver kube-controller-manager kube-scheduler
 systemctl start etcd kube-apiserver kube-controller-manager kube-scheduler
+
+sleep 10
+
+kubectl create -f kubernetes/
