50aaeaec92fe3500e6622fed1dd4dbcf835c4f26
Elson Rodriguez <elson.rodriguez@gmail.com>
2017-03-04 21:01:39 -0800

    Adding proxy to master

--- install-master.sh
+++ install-master.sh
@@ -1,6 +1,6 @@
-cp bin/{etcdctl,etcd,kubectl,kube-apiserver,kube-controller-manager,kube-scheduler} /usr/bin
+cp bin/{etcdctl,etcd,kubectl,kube-apiserver,kube-controller-manager,kube-scheduler,kube-proxy} /usr/bin
 
-cp units/{etcd.service,kube-apiserver.service,kube-controller-manager.service,kube-scheduler.service} /etc/systemd/system/
+cp units/{etcd.service,kube-apiserver.service,kube-controller-manager.service,kube-scheduler.service,kube-proxy.service} /etc/systemd/system/
 
 #Systemd routes not working, using hack for now
 #./generate-kube-routes.sh
@@ -13,7 +13,10 @@ systemctl enable kube-routes
 systemctl start kube-routes
 
 mkdir -p /etc/kubernetes/master
+mkdir -p /etc/kubernetes/node
 cp conf/{config.conf,apiserver.conf,controller-manager.conf,scheduler.conf} /etc/kubernetes/master
+cp conf/kube-proxy.conf /etc/kubernetes/node
+sed -i "/KUBE_MASTER_SERVER/d" /etc/kubernetes/node/kube-proxy.conf
 
 ./generate-kube-routes.sh
 cp units/kube-routes.network /etc/systemd/network
@@ -21,11 +24,12 @@ cp units/kube-routes.network /etc/systemd/network
 mkdir -p /etc/etcd
 cp conf/etcd.conf /etc/etcd
 
+mkdir -p /srv/kubernetes/kube-serviceaccount.key
 openssl genrsa -out /srv/kubernetes/kube-serviceaccount.key
 
 systemctl daemon-reload
-systemctl enable etcd kube-apiserver kube-controller-manager kube-scheduler
-systemctl start etcd kube-apiserver kube-controller-manager kube-scheduler
+systemctl enable etcd kube-apiserver kube-controller-manager kube-scheduler kube-proxy
+systemctl start etcd kube-apiserver kube-controller-manager kube-scheduler kube-proxy
 
 sleep 10
 
